21b04918bd91b5c12ab29054e6e8df5d87dc9251
Aayesha <aayasha@sociair.com>
2024-07-07 15:28:24 +0545

    changes made in js and html file

--- fetchdata.js
+++ fetchdata.js
@@ -1,37 +1,32 @@
-
-function fetchData() 
-{
-    for (i = 0; i < 12; i++) {
-        fetch("https://randomuser.me/api")
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (value) {
-                const user = value.results[0];//fetch data from API
-                const userInfo = extractuserinfo(user);// call function i.e extractuserinfo with parameter user
-                const usercontainer = document.createElement("div");// create a div and store in usercontainer variable
-                usercontainer.className = "usercard"; //creted a classname called usercard to style it using css
-                usercontainer.innerHTML= userInfo;//display data in html page
-                document.getElementById("container").appendChild(usercontainer);//displaying data in container
-            })
-            .catch(function (error) {
-                console.log("Error found:", error)
-                document.getElementById("error").innerHTML= "Error found"+" "+ error;
-            });
-    }
-}
-
+const fetchData = () => {
+  for (i = 0; i < 15; i++) {
+    fetch("https://randomuser.me/api")
+      .then((response) => response.json())
+      .then((value) => {
+        const user = value.results[0];
+        const userInformation = extractuserinfo(user);
+        const usercontainer = documnet.createElement("div");
+        usercontainer.className = "usercard";
+        usercontainer.innerHTML = userInformation;
+        document.getElementById("container").appendChild(usercontainer);
+      })
+      .catch((error) => {
+        console.log("error found:", error);
+        (document.getElementById("error").innerHTML = "Error found"), error;
+      });
+  }
+};
 function extractuserinfo(user) {
-    const image = user.picture.large;
-    const name = user.name;
-    const gender = user.gender;
-    const id= user.id;
-    const dob = user.dob;
-    const email = user.email;
-    const phone = user.phone;
-    const address = user.location;
+  const image = user.picture.large;
+  const name = user.name;
+  const gender = user.gender;
+  const id = user.id;
+  const dob = user.dob;
+  const email = user.email;
+  const phone = user.phone;
+  const address = user.location;
 
-    return`
+  return `
     <div>
         <img id="image" src="${image}"></img>
         <p>Name: ${name.first} ${name.last}</p>
@@ -41,8 +36,7 @@ function extractuserinfo(user) {
         <p>email: ${email}</p>
         <p>phoneno: ${phone}</p>
         <p>Address: ${address.city} ${address.state} ${address.country} ${address.street.name} ${address.street.number}</p>
-    </div>`
-    ;
+    </div>`;
 }
 
 fetchData();
